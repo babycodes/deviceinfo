fc92d8728ccf6c1aad6889589614668b735714cc
wahyu_kurniawan <wahyu.uptti@unej.ac.id>
2020-05-09 14:57:49 +0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const os = require("os");
 
 let user_log = {};
-let userOperator, osPlatform, model, network, ipWireless, ipLan, macAddress;
+let userOperator,
+  computerName,
+  osPlatform,
+  model,
+  network,
+  ipWireless,
+  ipLan,
+  macAddress;
 
 userOperator = "pengguna";
 model = os.cpus()[1].model;
 osPlatform = `${os.platform()} ${os.arch()}`;
 network = os.networkInterfaces();
+computerName = os.userInfo({ option: "utf8" }).username;
 
 if (os.platform == "linux") {
-  ipWireless = network.wlp3s0[0];
+  if (network.hasOwnProperty("wlp3s0")) {
+    ipWireless = network.wlp3s0[0];
+  } else if (network.hasOwnProperty("eth0")) {
+    ipWireless = network.eth0[0];
+  }
 } else {
   ipWireless = network.eth0[0];
 }
@@ -18,6 +30,7 @@ ipLan = network.lo[0].address;
 macAddress = ipWireless.mac;
 
 user_log = {
+  computerName,
   osPlatform,
   model,
   ipWireless: ipWireless.address,
